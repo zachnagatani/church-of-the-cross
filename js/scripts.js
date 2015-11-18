a7c773187e29832a9edbf04d31425a4604138ac2
znagatani <znagatani@gmail.com>
2015-11-18 12:15:47 -0800

    not yellow

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,48 @@ function formSuccess(){
 function submitMSG(){
       $( "#msgFail" ).removeClass( "hidden" );
 }
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+// Parallax Effect
+$(function() {
+  
+  // Cache the Window object
+  var $window = $(window);
+  
+  // Parallax Backgrounds
+  // Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+  
+  $('section[data-type="background"]').each(function(){
+    var $bgobj = $(this); // assigning the object
+    
+    $(window).scroll(function() {
+    
+      // Scroll the background at var speed
+      // the yPos is a negative value because we're scrolling it UP!                
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+      
+      // Put together our final background position
+      var coords = '50% '+ yPos + 'px';
+      
+      // Move the background
+      $bgobj.css({ backgroundPosition: coords });
+      
+    }); // end window scroll
+  });
+  
+});
