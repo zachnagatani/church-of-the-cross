434de71482f7172553df93481ef612608f505a30
znagatani <znagatani@gmail.com>
2015-11-17 21:00:41 -0800

    stuff ready for launch

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-var $height = $("#footer").height();
+$("#contact-form").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
 
-$(".body").css({'padding-bottom' : $height;});
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#contact-name").val();
+    var email = $("#contact-email").val();
+    var message = $("#contact-message").val();
+ 
+
+    $.ajax({
+      type: "POST",
+      url: "php/process.php",
+      data: "name=" + name + "&email=" + email + "&message=" + message,
+      success : function(text){
+          if (text == "success"){
+              formSuccess();
+          } else {
+              submitMSG(false,text);
+          }
+      }
+  });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
+
+function submitMSG(){
+      $( "#msgFail" ).removeClass( "hidden" );
+}
